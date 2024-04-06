e575f7e48cf765483096dfe3cad7e6cd604cc0d3
Jarred Norris <jarred.norris1@gmail.com>
2024-04-06 04:39:42 +0100

    :bug: remove not implemented timeline showcase context

--- src/components/timeline-showcase-context.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-
-const TimelineShowcaseContext = React.createContext<{}>(null)
-
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,7 @@
 import { neon } from "@neondatabase/serverless";
-const sql = neon(process.env.DATABASE_URL);
+const sql = neon(process.env.DATABASE_URL ? process.env.DATABASE_URL : "");
 
 export async function helloWorld() {
-	const [dbResponse] = await sql`SELECT NOW();`;
-	return dbResponse;
+  const [dbResponse] = await sql`SELECT NOW();`;
+  return dbResponse;
 }
