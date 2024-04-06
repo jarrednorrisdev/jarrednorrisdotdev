eb60a671a0cf62ad95d71fbccc2165094e09e7e1
Jarred Norris <jarred.norris1@gmail.com>
2024-04-06 05:24:12 +0100

    :bug: fix incorrect env.ts

--- src/env.ts
+++ src/env.ts
@@ -3,12 +3,15 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    VERCEL_ENV: z.enum(["development", "test", "production"]),
+    DATABASE_URL: z.string().url(),
   },
   client: {
-    // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    NEXT_PUBLIC_VERCEL_ENV: z.enum(["development", "test", "production"]),
+    NEXT_PUBLIC_VERCEL_URL: z.string().url(),
   },
   runtimeEnv: {
-    VERCEL_ENV: process.env.NEXT_PUBLIC_VERCEL_ENV,
+    NEXT_PUBLIC_VERCEL_ENV: process.env.NEXT_PUBLIC_VERCEL_ENV,
+    NEXT_PUBLIC_VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
   },
 });
