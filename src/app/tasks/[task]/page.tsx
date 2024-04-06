39e1830ee207111f6856ce0b97540952052ee5c1
Jarred Norris <jarred.norris1@gmail.com>
2024-04-06 07:09:59 +0100

    :art: add error to tasks route

--- src/app/tasks/[task]/page.tsx
+++
@@ -1,9 +0,0 @@
-// "use client";
-
-export default function TaskDetail() {
-  return (
-    <main className="flex flex-col gap-8 border-accent bg-background p-6 md:container md:mx-auto">
-      Tasks Page
-    </main>
-  );
-}
