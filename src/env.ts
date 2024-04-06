b0fe77a9820d706f7717e4106c6e1303e2f0bf11
Jarred Norris <jarred.norris1@gmail.com>
2024-04-06 06:59:18 +0100

    :bug: remove zod url check for env next domains

--- src/env.ts
+++ src/env.ts
@@ -7,8 +7,8 @@ export const env = createEnv({
   },
   client: {
     NEXT_PUBLIC_VERCEL_ENV: z.enum(["development", "preview", "production"]),
-    NEXT_PUBLIC_VERCEL_URL: z.string().url("invalid URL for NEXT_PUBLIC_VERCEL_URL"),
-    NEXT_PUBLIC_VERCEL_BRANCH_URL: z.string().url("invalid URL for NEXT_PUBLIC_VERCEL_BRANCH_URL"),
+    NEXT_PUBLIC_VERCEL_URL: z.string(),
+    NEXT_PUBLIC_VERCEL_BRANCH_URL: z.string(),
   },
   experimental__runtimeEnv: {
     NEXT_PUBLIC_VERCEL_ENV: process.env.NEXT_PUBLIC_VERCEL_ENV,
