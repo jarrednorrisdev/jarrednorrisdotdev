0fc7c3785be0a6be6ad81fb56eb4fbb8b3a7cc12
Jarred Norris <jarred.norris1@gmail.com>
2024-04-06 04:31:52 +0100

    :bug: fix more types

--- src/app/api/tasks/route.ts
+++ src/app/api/tasks/route.ts
@@ -1,5 +1,4 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import { headers } from "next/headers";
 
 interface Task {
@@ -7,10 +6,10 @@ interface Task {
   name: string;
 }
 
-export async function GET(req: NextApiRequest, res: NextApiResponse<Task>) {
+export async function GET(req: NextRequest, res: NextResponse<Task>) {
   console.log("request", req);
   console.log("context", res);
-  return NextResponse.json({
+  return Response.json({
     tasks: [
       { id: "0", name: "taskname" },
       { id: "1", name: "taskname" },
@@ -18,7 +17,7 @@ export async function GET(req: NextApiRequest, res: NextApiResponse<Task>) {
   });
 }
 
-export async function POST(req: NextApiRequest) {
+export async function POST(req: NextRequest) {
   const headersList = headers();
 
   const contentType = headersList.get("content-type");
@@ -27,6 +26,6 @@ export async function POST(req: NextApiRequest) {
   if (contentType !== "application/json") {
     return NextResponse.json("invalid content typee", { status: 400 });
   }
-  const data = await req.read();
-  return NextResponse.json(data, { status: 201 });
+  const data = await req.json();
+  return Response.json(data, { status: 201 });
 }
