3458a4368ff82e65b3087879798399c6a2917af5
Jarred Norris <jarred.norris1@gmail.com>
2024-06-10 10:37:12 +0100

    fix incorrect borders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,10 @@ export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
   return (
-    <main className="flex flex-grow flex-wrap gap-4 p-4 md:container md:mx-auto md:overflow-auto">
-      <div className="flex flex-row flex-wrap  gap-4 p-4 lg:flex-nowrap">
-        <MainHero className="flex flex-grow gap-8 rounded border bg-background px-4 py-2" />
-        <PreviousExperienceShowcase className="flex flex-grow flex-wrap gap-2 rounded-md border bg-background px-5 py-3  md:overflow-hidden  " />
+    <main className="flex flex-grow flex-wrap gap-4 p-4 md:container md:mx-auto md:overflow-auto ">
+      <div className=" flex flex-row  flex-wrap gap-4 rounded border  p-4 lg:flex-nowrap">
+        <MainHero className="flex flex-grow gap-8 rounded border bg-background px-4 py-2 lg:w-1/3" />
+        <PreviousExperienceShowcase className="flex flex-grow flex-wrap gap-2 rounded border bg-background px-5 py-3  md:overflow-hidden  " />
       </div>
     </main>
   );
